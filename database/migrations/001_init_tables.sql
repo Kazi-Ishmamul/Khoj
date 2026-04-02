a05ff6d4736911973ca27b4c758afe2285550a8b
Kazi Ishmamul Haque <163518550+Kazi-Ishmamul@users.noreply.github.com>
2026-04-02 20:35:45 +0600

    Modify database schema_02

--- database/migrations/001_init_tables.sql
+++ database/migrations/001_init_tables.sql
@@ -1,41 +1,65 @@
 CREATE DATABASE IF NOT EXISTS `khoj` 
-    DEFAULT CHARACTER SET latin1 
-    COLLATE latin1_danish_ci
+    DEFAULT CHARACTER SET utf8mb4 
+    COLLATE utf8mb4_unicode_ci;
 
-USE `khoj`
-
-Drop table IF Exists 'user_info'
-Drop table IF Exists 'users'
+USE `khoj`;
 
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    address TEXT NOT NULL,
-    pic_url VARCHAR(512) DEFAULT 'https://res.cloudinary.com/dait0sacc/image/upload/v1774704629/k7ygnoel72ychr8ico6n.png',
-    password VARCHAR(255) NOT NULL,
-    role ENUM('user', 'admin') DEFAULT 'user',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+DROP TABLE IF EXISTS `user_info`;
+DROP TABLE IF EXISTS `personal_access_tokens`;
+DROP TABLE IF EXISTS `migrations`;
+DROP TABLE IF EXISTS `users`;
 
 
-CREATE TABLE user_info (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    bio TEXT,
+CREATE TABLE `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `email` VARCHAR(150) NOT NULL UNIQUE,
+    `address` TEXT NOT NULL,
+    `pic_url` VARCHAR(512) DEFAULT 'https://res.cloudinary.com/dait0sacc/image/upload/v1774704629/k7ygnoel72ychr8ico6n.png',
+    `password` VARCHAR(255) NOT NULL,
+    `role` ENUM('user', 'admin') DEFAULT 'user',
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
 
-    fb_url VARCHAR(255),
-    x_url VARCHAR(255),
-    insta_url VARCHAR(255),
-    linkedin_url VARCHAR(255),
 
-    items_lost_count INT DEFAULT 0,
-    items_found_count INT DEFAULT 0,
-    report_strikes INT DEFAULT 0,
-
+CREATE TABLE `user_info` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `bio` TEXT,
+    `fb_url` VARCHAR(255),
+    `x_url` VARCHAR(255),
+    `insta_url` VARCHAR(255),
+    `linkedin_url` VARCHAR(255),
+    `items_lost_count` INT DEFAULT 0,
+    `items_found_count` INT DEFAULT 0,
+    `report_strikes` INT DEFAULT 0,
     CONSTRAINT fk_user_stats FOREIGN KEY (user_id) 
         REFERENCES users(id) 
         ON DELETE CASCADE
-)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `migrations` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` VARCHAR(255) NOT NULL,
+  `batch` INT(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `personal_access_tokens` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tokenable_type` VARCHAR(255) NOT NULL,
+  `tokenable_id` BIGINT(20) UNSIGNED NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `token` VARCHAR(64) NOT NULL,
+  `abilities` TEXT DEFAULT NULL,
+  `last_used_at` TIMESTAMP NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT NULL,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tokens_token_unique` (`token`),
+  INDEX `tokens_tokenable_index` (`tokenable_type`, `tokenable_id`)
+) ENGINE=InnoDB;
